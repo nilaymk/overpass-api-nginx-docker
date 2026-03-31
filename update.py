9c54632af71fc9b28d8a672ba8d2e393b8c6d99b
Nilay Kothari <nkothari@optiml.com>
2026-04-01 01:20:22 +0200

    Update the template to use correc tools

--- update.py
+++ update.py
@@ -1,10 +1,9 @@
+import argparse
 import html.parser
-import os
-import pathlib
-import shutil
 import urllib.request
 
 url = "http://dev.overpass-api.de/releases/"
+
 
 
 class VersionFinder(html.parser.HTMLParser):
@@ -23,13 +22,12 @@ class VersionFinder(html.parser.HTMLParser):
                 self.versions.append(version)
 
 
-def main():
+def fetch_versions():
     parser = VersionFinder()
     response = urllib.request.urlopen(url)
     data = response.read().decode(response.headers.get_content_charset())
     parser.feed(data)
-    with open("Dockerfile.template") as f:
-        template = f.read()
+    versions = []
     for ver in parser.versions:
         if any((ver.startswith(x) for x in ('0.6', 'eta', '0.7.1', '0.7.2', '0.7.3', '0.7.4', '0.7.50', '0.7.52',
                                             '0.7.54.11',  # invalid CRC in archive
@@ -38,15 +36,55 @@ def main():
                 ver == '0.7':
             # ignore old releases
             continue
-        if os.path.exists(ver):
-            shutil.rmtree(ver)
-        os.mkdir(ver)
-        with open(pathlib.Path(ver) / "Dockerfile", "w+") as f:
-            f.write(template.format(version=ver))
-        #for i in ("etc", "bin"):
-        #    shutil.copytree(i, pathlib.Path(ver) / i)
-        #shutil.copyfile("docker-entrypoint.sh", pathlib.Path(ver) / "docker-entrypoint.sh")
-        #shutil.copyfile("requirements.txt", pathlib.Path(ver) / "requirements.txt")
+        try:
+            key = tuple(int(x) for x in ver.split('.'))
+        except ValueError:
+            continue
+        versions.append((key, ver))
+    return [ver for _, ver in sorted(versions)]
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(
+        description="Generate Dockerfiles from a template for overpass-api releases."
+    )
+    arg_parser.add_argument(
+        "template",
+        help="Path to the Dockerfile template file.",
+    )
+    arg_parser.add_argument(
+        "--version",
+        required=True,
+        help="Version to generate for, or 'latest' to use the latest release.",
+    )
+    arg_parser.add_argument(
+        "--output",
+        default="Dockerfile",
+        help="Output path for the generated Dockerfile (default: Dockerfile).",
+    )
+    arg_parser.add_argument(
+        "--param",
+        metavar="KEY=VALUE",
+        action="append",
+        default=[],
+        help="Additional template parameters as KEY=VALUE pairs. May be specified multiple times.",
+    )
+    args = arg_parser.parse_args()
+
+    extra_params = {}
+    for item in args.param:
+        if "=" not in item:
+            arg_parser.error(f"--param must be in KEY=VALUE format, got: {item!r}")
+        key, _, value = item.partition("=")
+        extra_params[key] = value
+
+    ver = fetch_versions()[-1] if args.version == "latest" else args.version
+
+    with open(args.template) as f:
+        template = f.read()
+
+    with open(args.output, "w+") as f:
+        f.write(template.format(version=ver, **extra_params))
 
 
 if __name__ == '__main__':
